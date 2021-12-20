eaabae5496190fe50ed5f33b28b5ac1b6dee2e9d
Katia Silva <katiasilva2235@gmail.com>
2021-12-20 09:26:28 -0300

    Projeto java

---
+++ Pessoas.java
@@ -0,0 +1,39 @@
+package aula2;
+
+public class Pessoas {
+
+
+	public static void main(String[] args) {
+		
+		 String nome;
+		 nome = "Katia Silva";
+		 System.out.println( "nome:" + nome);
+		 int idade = 33;
+		 String naturalidade = "Vitoria de Santo A";
+		boolean loira = false;
+		char sexo = 'F';
+		String estadocivil = "Solteira";
+		double altura = 1.55;
+		System.out.println("idade:" + idade);
+		System.out.println("naturalidade:" + naturalidade);
+		System.out.println("loira:" + loira);
+		System.out.println("sexo:" + sexo);
+		System.out.println("estado civil:" + estadocivil );
+		System.out.println("altura:" + altura);
+		
+		
+				 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+
+	}
+
+}
